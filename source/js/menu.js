a8ba0f9c7c73fb1cd59534f6112d54225ecb4a6c
hazerman <49827421+hazerman@users.noreply.github.com>
2020-05-07 14:11:30 +0500

    Добавляет скрытие меню при нажатии на элемент

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,15 +1,26 @@
 'use strict';
 
 (function () {
+  var TABLET_WIDTH = 768;
   var menuButton = document.querySelector('.header__button-toggle');
   var menu = document.querySelector('.header__nav');
+  var menuLinks = menu.querySelectorAll('a');
 
   menu.classList.remove('header__nav--no-js');
   menuButton.classList.remove('header__button-toggle--no-js');
   menuButton.removeAttribute('aria-hidden');
 
   menuButton.addEventListener('click', function () {
-    menuButton.classList.toggle('header__button-toggle--show')
+    menuButton.classList.toggle('header__button-toggle--show');
     menu.classList.toggle('header__nav--show');
   });
+
+  if (window.innerWidth <= TABLET_WIDTH) {
+    menuLinks.forEach(function (item) {
+      item.addEventListener('click', function () {
+        menuButton.classList.toggle('header__button-toggle--show');
+        menu.classList.toggle('header__nav--show');
+      });
+    });
+  }
 })();
